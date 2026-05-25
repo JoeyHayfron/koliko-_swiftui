f0b4f314576018232763e635838653cb50aab86d
Joseph Hayfron Acquah <joeacq81@gmail.com>
2026-05-25 00:11:44 +0000

    Initial Commit

---
+++ koliko+/koliko_App.swift
@@ -0,0 +1,17 @@
+//
+//  koliko_App.swift
+//  koliko+
+//
+//  Created by Joseph Acquah on 25/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct koliko_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
